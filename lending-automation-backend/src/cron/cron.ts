c5bdb4eb9f72192c3ad89eae8ec5c04cad0def98
SarveshLimaye <sarveshlimaye2002@gmail.com>
2025-10-26 00:48:12 +0530

    feat:initial scheduler logic done

---
+++ lending-automation-backend/src/cron/cron.ts
@@ -0,0 +1,79 @@
+import { CHAINLINK_ABI } from "../utils/abi/chainlink";
+import Order from "../models/Order";
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+import { AaveV3BaseSepolia } from "@bgd-labs/aave-address-book";
+import { aaveOperation } from "../lit-automated-jobs/aave/aaveOperations";
+
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.BASE_SEPOLIA_RPC
+);
+
+export const rebalancePositions = async () => {
+  try {
+    console.log("Rebalancing positions...");
+    const orders = await Order.find();
+    console.log("Fetched orders:", orders.length);
+    for (const order of orders) {
+      console.log("Processing order:", order);
+      const { ethAddress, collateralAsset, loanAsset, healthRatioToMaintain } =
+        order;
+
+      console.log(collateralAsset, loanAsset);
+      const collateralOracleAddress = getOracleAddress(
+        collateralAsset as string
+      );
+
+      console.log("Collateral Oracle Address:", collateralOracleAddress);
+
+      const loanOracleAddress = getOracleAddress(loanAsset as string);
+
+      const oracleAddress = collateralOracleAddress && loanOracleAddress;
+      if (!oracleAddress) continue;
+
+      const [collateralPrice, loanPrice] = await Promise.all([
+        fetchOraclePrice(collateralOracleAddress as string),
+        fetchOraclePrice(loanOracleAddress as string),
+      ]);
+
+      console.log(
+        `Collateral Price: ${collateralPrice}, Loan Price: ${loanPrice}`
+      );
+
+      if (order.orderType === "takeProfit") {
+        // to-do: implement take profit logic
+      } else if (order.orderType === "stopLoss") {
+        // to-do: implement stop loss logic
+      } else if (order.orderType === "rebalance") {
+        // to-do: implement rebalance logic
+      }
+    }
+  } catch (error) {}
+};
+
+const getOracleAddress = (assetAddress: string) => {
+  switch (assetAddress.toLowerCase()) {
+    case AaveV3BaseSepolia.ASSETS.USDC.UNDERLYING.toLowerCase():
+      return AaveV3BaseSepolia.ASSETS.USDC.ORACLE;
+    case AaveV3BaseSepolia.ASSETS.USDT.UNDERLYING.toLowerCase():
+      return AaveV3BaseSepolia.ASSETS.USDT.ORACLE;
+    case AaveV3BaseSepolia.ASSETS.cbETH.UNDERLYING.toLowerCase():
+      return AaveV3BaseSepolia.ASSETS.cbETH.ORACLE;
+    case AaveV3BaseSepolia.ASSETS.WBTC.UNDERLYING.toLowerCase():
+      return AaveV3BaseSepolia.ASSETS.WBTC.ORACLE;
+    default:
+      return null;
+  }
+};
+
+const fetchOraclePrice = async (oracleAddress: string): Promise<number> => {
+  const oracle = new ethers.Contract(oracleAddress, CHAINLINK_ABI, provider);
+
+  const [, answer] = await oracle.latestRoundData();
+  const decimals = await oracle.decimals();
+
+  return Number(answer) / Math.pow(10, Number(decimals));
+};
